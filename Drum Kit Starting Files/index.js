900789ac27b2af2add9ef6629f022ac9d7add849
SHIBINSHA <shibin24888@gmail.com>
2023-11-12 23:54:30 +0530

    click function

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,23 +1,28 @@
 var audio = ["sounds/crash.mp3", "sounds/kick-bass.mp3", "sounds/snare.mp3", "sounds/tom-1.mp3", "sounds/tom-2.mp3", "sounds/tom-3.mp3", "sounds/tom-4.mp3"];
 
-document.addEventListener("keydown", function() {
-    var keyPressed = event.key.toLowerCase(); // Convert to lowercase aassafor case-insensitivity
-    
-    // Map keys to drum sounds
+function playDrumSound(index) {
+    var audioPlayer = new Audio(audio[index]);
+    audioPlayer.play();
+}
+
+// Click Event Listener
+document.addEventListener("click", function(event) {
+    var targetElement = event.target;
+    if (targetElement.classList.contains("drum")) {
+        var index = Array.from(document.querySelectorAll(".drum")).indexOf(targetElement);
+        playDrumSound(index);
+    }
+});
+
+// Keydown Event Listener
+document.addEventListener("keydown", function(event) {
+    var keyPressed = event.key.toLowerCase();
+
     var keyMappings = {
-        'w': 0,  // Press '1' for the first drum sound
-        'a': 1,  // Press '2' for the second drum sound
-        's': 2,  // Press '3' for the third drum sound
-        'd': 3,  // Press '4' for the fourth drum sound
-        'j': 4,  // Press '5' for the fifth drum sound
-        'k': 5,  // Press '6' for the sixth drum sound
-        'l': 6   // Press '7' for the seventh drum sound
+        'w': 0, 'a': 1, 's': 2, 'd': 3, 'j': 4, 'k': 5, 'l': 6
     };
 
-    // Check if the pressed key is mapped to a drum sound
     if (keyMappings[keyPressed] !== undefined) {
-        var index = keyMappings[keyPressed];
-        var audioPlayer = new Audio(audio[index]);
-        audioPlayer.play();
+        playDrumSound(keyMappings[keyPressed]);
     }
 });
